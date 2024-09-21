e7b8cf6325fe72156bbc6892c50c583271243494
Aditi Mangesh Joshi <117927433+aditimj@users.noreply.github.com>
2024-09-21 16:31:19 -0400

    [1211] [Time Beats: 74.45%] - LeetPush

--- 1211-Queries-Quality-and-Percentage.sql
+++ 1211-Queries-Quality-and-Percentage.sql
@@ -2,4 +2,6 @@
 select query_name, 
     round(Avg(rating/position),2) as quality, 
     round((count(CASE WHEN rating < 3 then 1 end) / count(query_name)) * 100,2) as poor_query_percentage 
-from Queries where query_name is not null group by query_name
+from Queries 
+where query_name is not null 
+group by query_name
