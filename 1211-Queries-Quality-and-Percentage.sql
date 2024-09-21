3db046d4242a5b710eed08e048a646c8fa333721
Aditi Mangesh Joshi <117927433+aditimj@users.noreply.github.com>
2024-09-21 16:30:15 -0400

    [1211] [Time Beats: 64.48%] - LeetPush

---
+++ 1211-Queries-Quality-and-Percentage.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select query_name, 
+    round(Avg(rating/position),2) as quality, 
+    round((count(CASE WHEN rating < 3 then 1 end) / count(query_name)) * 100,2) as poor_query_percentage 
+from Queries where query_name is not null group by query_name
