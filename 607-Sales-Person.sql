58d9dfe40095a5b10d1b6b29b76afdb0baa54414
Aditi Mangesh Joshi <117927433+aditimj@users.noreply.github.com>
2024-09-21 19:16:29 -0400

    [607] [Time Beats: 86.27%] - LeetPush

---
+++ 607-Sales-Person.sql
@@ -0,0 +1,8 @@
+SELECT SalesPerson.name 
+FROM SalesPerson
+WHERE SalesPerson.sales_id NOT IN (
+    SELECT Orders.sales_id
+    FROM Orders
+    JOIN Company ON Orders.com_id = Company.com_id
+    WHERE Company.name = 'RED'
+);
