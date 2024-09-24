9e574eb35a7cc24f2a8f0f718b836dc5ff02f8d1
Aditi Mangesh Joshi <117927433+aditimj@users.noreply.github.com>
2024-09-23 21:39:29 -0400

    [1693] [Time Beats: 42.90%] - LeetPush

---
+++ 1693-Daily-Leads-and-Partners.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select date_id, make_name,  Count(DISTINCT lead_id) as unique_leads, 
+    COUNT( DISTINCT partner_id ) as unique_partners
+from DailySales
+group by date_id, make_name
