b7dd47e253fd5c44a0b56dbf3a165faa6a65a2d4
Aditi Mangesh Joshi <117927433+aditimj@users.noreply.github.com>
2024-09-23 20:59:55 -0400

    [1527] [Time Beats: 71.92%] - LeetPush

--- 1527-Patients-With-a-Condition.sql
+++ 1527-Patients-With-a-Condition.sql
@@ -1,1 +1,2 @@
-SELECT patient_id, patient_name, conditions FROM Patients WHERE conditions LIKE 'DIAB1%' OR conditions LIKE '% DIAB1%' OR conditions LIKE '% DIAB1';     
+SELECT patient_id, patient_name, conditions 
+FROM Patients WHERE conditions LIKE 'DIAB1%' OR conditions LIKE '% DIAB1%' OR conditions LIKE '% DIAB1';     
