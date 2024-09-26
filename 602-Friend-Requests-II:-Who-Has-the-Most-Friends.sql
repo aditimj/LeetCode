4c5647c7a2c2d351a3a4ca5eac22cbd800a2e48d
Aditi Mangesh Joshi <117927433+aditimj@users.noreply.github.com>
2024-09-26 01:07:15 -0400

    [602] [Time Beats: 91.90%] - LeetPush

---
+++ 602-Friend-Requests-II:-Who-Has-the-Most-Friends.sql
@@ -0,0 +1,7 @@
+SELECT id, COUNT(*) AS num
+FROM (
+    SELECT requester_id AS id FROM RequestAccepted
+    UNION ALL
+    SELECT accepter_id AS id FROM RequestAccepted
+) AS combined
+GROUP BY id ORDER BY num DESC LIMIT 1
