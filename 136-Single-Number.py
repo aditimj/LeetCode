ac4819afbffd374cf49ae723007a56b1c0fad9f8
Aditi Mangesh Joshi <117927433+aditimj@users.noreply.github.com>
2024-09-30 15:59:33 -0400

    [136] [Time Beats: 68.59%] [Memory Beats: 58.53%] - LeetPush

--- 136-Single-Number.py
+++ 136-Single-Number.py
@@ -2,5 +2,8 @@ class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         res = 0
         for i in nums:
+            # print('i',i)
+            # print(f'res {res} ^ i {i}')
             res ^= i
+            # print('res',res)
         return res   
