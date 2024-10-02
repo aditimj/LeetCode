a5201ad6c28c31cd5ebb3f07b9a10769286e29c4
Aditi Mangesh Joshi <117927433+aditimj@users.noreply.github.com>
2024-10-01 21:13:20 -0400

    [349] [Time Beats: 5.17%] [Memory Beats: 93.59%] - LeetPush

---
+++ 349-Intersection-of-Two-Arrays.py
@@ -0,0 +1,11 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+
+        ans = []
+        for i in range(len(nums1)):
+            for j in range(len(nums2)):
+                if nums1[i]==nums2[j] and nums1[i] not in ans:
+                    ans.append(nums1[i])
+        return ans
+                    
+        
