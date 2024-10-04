f870a198485738488741db07f90ca4f127fb52da
Aditi Mangesh Joshi <117927433+aditimj@users.noreply.github.com>
2024-10-04 19:46:46 -0400

    [383] [Time Beats: 43.48%] [Memory Beats: 27.76%] - LeetPush

---
+++ 383-Ransom-Note.py
@@ -0,0 +1,24 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        rmap = {}
+        mmap = {}
+        for i in ransomNote:
+            if i in rmap:
+                rmap[i] += 1
+            else:
+                rmap[i] = 1
+        for i in magazine:
+            if i in mmap:
+                mmap[i] += 1
+            else:
+                mmap[i] = 1
+        for i in rmap:
+            if rmap[i] > mmap.get(i,0):
+                return False
+        return True
+
+
+        return 0
+
+
+        
