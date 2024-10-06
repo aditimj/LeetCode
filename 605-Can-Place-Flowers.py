1efbdb67a937033f0c1cc63ae54b640fa891097a
Aditi Mangesh Joshi <117927433+aditimj@users.noreply.github.com>
2024-10-06 14:09:27 -0400

    [605] [Time Beats: 88.38%] [Memory Beats: 26.92%] - LeetPush

--- 605-Can-Place-Flowers.py
+++ 605-Can-Place-Flowers.py
@@ -1,11 +1,22 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:        
-        cnt = 0            
+        cnt = 0   
+        if len(flowerbed) > 2: 
+            if flowerbed[0] == 0 and flowerbed[1] == 0:
+                cnt += 1
+                flowerbed[0] = 1
+            if flowerbed[-1] == 0 and flowerbed[-2] == 0:
+                cnt+= 1
+                flowerbed[-1] = 1
+        if flowerbed[0] == 0 and len(flowerbed) ==1:
+            cnt = 1
+        if flowerbed[0] == 1 and len(flowerbed) ==1:
+            cnt = 0
+        if cnt>=n:
+            return True
         for i in range(len(flowerbed)):
             if flowerbed[i] == 0:
-                prev_empty = (i == 0) or (flowerbed[i - 1] == 0) 
-                next_empty = (i == len(flowerbed) - 1) or (flowerbed[i + 1] == 0) 
-                if prev_empty and next_empty:
+                if (flowerbed[i - 1] == 0) and (flowerbed[i + 1] == 0):
                     flowerbed[i] = 1  
                     cnt += 1  
                     if cnt >= n:
