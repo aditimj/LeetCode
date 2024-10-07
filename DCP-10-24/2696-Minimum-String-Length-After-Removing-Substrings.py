958f17dc7eab68d570ab1b7a110e9bf4a8fc3b3d
Aditi Mangesh Joshi <117927433+aditimj@users.noreply.github.com>
2024-10-07 13:16:59 -0400

    [2696] [Time Beats: 81.05%] [Memory Beats: 41.77%] - LeetPush

---
+++ DCP-10-24/2696-Minimum-String-Length-After-Removing-Substrings.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minLength(self, s: str) -> int:
+        stack = []
+
+        for i in s:
+            if stack and ((stack[-1] == 'A' and i == "B") or (stack[-1] == "C" and i == "D")):
+                stack.pop()
+            else:
+                stack.append(i)
+        return len(stack)
+
+
+        
