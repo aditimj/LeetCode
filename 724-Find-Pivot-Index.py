f8ebf781363af52c24398fd1dcfac67243b349fd
Aditi Mangesh Joshi <117927433+aditimj@users.noreply.github.com>
2024-10-12 01:14:07 -0400

    [724] [Time Beats: 66.88%] [Memory Beats: 87.98%] - LeetPush

---
+++ 724-Find-Pivot-Index.py
@@ -0,0 +1,13 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        tot = sum(nums)
+        left_sum = 0
+        for i in range(0,len(nums)):
+
+            if left_sum == tot - left_sum - nums[i]:
+                return i
+            left_sum = left_sum + nums[i]
+    
+        return -1
+
+        
