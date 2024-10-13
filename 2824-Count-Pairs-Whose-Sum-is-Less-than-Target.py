68646701ab21495acb43d0a7d6d11420a39cc801
Aditi Mangesh Joshi <117927433+aditimj@users.noreply.github.com>
2024-10-13 19:38:03 -0400

    [2824] [Time Beats: 82.98%] [Memory Beats: 6.67%] - LeetPush

--- 2824-Count-Pairs-Whose-Sum-is-Less-than-Target.py
+++ 2824-Count-Pairs-Whose-Sum-is-Less-than-Target.py
@@ -1,39 +1,34 @@
 class Solution:
     def countPairs(self, nums: List[int], target: int) -> int:
         nums = sorted(nums)
-        cnt = 0
         i = 0
-        j = len(nums) - 1
-        while i < j:
+        j = 1   
+        cnt = 0     
+        while i < j and j < len(nums):
             if nums[i] + nums[j] < target:
-                cnt += (j-i)
-                i +=1
-            else:
-                j -= 1
+                cnt += 1
+                j += 1
+                if j == len(nums) - 1 and i != len(nums) - 2:
+                    if nums[i] + nums[j] < target:
+                        cnt += 1
+                        i += 1
+                        j = i + 1
+            else: 
+                i += 1
+                j = i + 1
         return cnt
+
             
-
-
-
-
+        # nums = sorted(nums)
+        # cnt = 0
         # i = 0
-        # j = 1        
-        # while i < j and j < len(nums):
-        #     print(i,j,nums[i] + nums[j])
+        # j = len(nums) - 1
+        # while i < j:
         #     if nums[i] + nums[j] < target:
-        #         cnt += 1
-        #         j += 1
-        #         if j == len(nums) - 1 and i != len(nums) - 2:
-        #             if nums[i] + nums[j] < target:
-        #                 cnt += 1
-        #                 i += 1
-        #                 j = i + 1
-        #     else: 
-        #         i += 1
-        #         j = i + 1
+        #         cnt += (j-i)
+        #         i +=1
+        #     else:
+        #         j -= 1
         # return cnt
-
-            
-
 
         
