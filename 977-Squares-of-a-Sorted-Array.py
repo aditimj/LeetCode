f5b14ecf0a47f941ce7bef9bd15909b802046400
Aditi Mangesh Joshi <117927433+aditimj@users.noreply.github.com>
2024-10-12 21:28:42 -0400

    [977] [Time Beats: 57.68%] [Memory Beats: 81.56%] - LeetPush

--- 977-Squares-of-a-Sorted-Array.py
+++ 977-Squares-of-a-Sorted-Array.py
@@ -1,7 +1,27 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
+
+        left = 0
+        right = len(nums) - 1
         ans = []
-        for i in range(len(nums)):
-            ans.append(nums[i] * nums[i])
-        return sorted(ans)
+        while left <= right:
+            if abs(nums[left]) > abs(nums[right]):
+                ans.append(nums[left] ** 2)
+                left += 1
+            else:
+                ans.append(nums[right] ** 2)
+                right -= 1
+        return reversed(ans)
+
+
+
+
+
+            
+
+
+        # ans = []
+        # for i in range(len(nums)):
+        #     ans.append(nums[i] * nums[i])
+        # return sorted(ans)
         
