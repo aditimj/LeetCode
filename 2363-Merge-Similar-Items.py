08caf539ba8d3652927797f773b9e1545299ac44
Aditi Mangesh Joshi <117927433+aditimj@users.noreply.github.com>
2024-10-29 22:29:21 -0400

    [2363] [Time Beats: 86.42%] [Memory Beats: 35.49%] - LeetPush

--- 2363-Merge-Similar-Items.py
+++ 2363-Merge-Similar-Items.py
@@ -1,23 +1,21 @@
 class Solution:
     def mergeSimilarItems(self, items1: List[List[int]], items2: List[List[int]]) -> List[List[int]]:
-        item1map = {}
-        item2map = {}
+        item_map = {}
+
         for v, w in items1:
-            item1map[v] = w
-        for v, w in items2:
-            item2map[v] = w
-        sitem2map = dict(sorted(item2map.items(), key = lambda item:item[0]))
-        sitem1map = dict(sorted(item1map.items(), key = lambda item:item[0]))
-        ans = []
-        for key, val in sitem1map.items():
-            if key in sitem2map:
-                ans.append([key, sitem1map[key] + sitem2map[key]])
+            if v in item_map:
+                item_map[v] += w 
             else:
-                ans.append([key,sitem1map[key]])
-        for key, val in sitem2map.items():
-            if key not in sitem1map:
-                ans.append([key, val])
-        ans.sort(key=lambda item: item[0])
+                item_map[v] = w 
+
+        for v, w in items2:
+            if v in item_map:
+                item_map[v] += w 
+            else:
+                item_map[v] = w  
+
+        ans = sorted(item_map.items())  
+
         return ans
 
         
