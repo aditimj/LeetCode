ff37f8c539e9e7bf036f45e14f627da3a93e609d
Aditi Mangesh Joshi <117927433+aditimj@users.noreply.github.com>
2024-11-07 17:23:33 -0500

    [1581] [Time Beats: 47.18%] - LeetPush

--- 1581-Customer-Who-Visited-but-Did-Not-Make-Any-Transactions.sql
+++ 1581-Customer-Who-Visited-but-Did-Not-Make-Any-Transactions.sql
@@ -3,4 +3,5 @@ select Visits.customer_id, count(Visits.customer_id) as count_no_trans from Visi
 left join Transactions on
 Visits.visit_id = Transactions.visit_id
 where Transactions.transaction_id is null
-group by Visits.customer_id
+group by customer_id 
+ 
